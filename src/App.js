752c96e10b4b37e156a0b5c63eb982785b5a0a44
faisalprady <faisalprady@gmail.com>
2019-11-12 17:07:30 +0100

    change some directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 // import './App.scss';
-import Hero from './components/Hero';
+import Hero from './components/Hero/Hero';
 import 'tachyons';
 
 function App() {

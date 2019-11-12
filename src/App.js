36a99f2989d6102f1cb67eabb86a95cf07635a8c
faisalprady <faisalprady@gmail.com>
2019-11-12 17:43:43 +0100

    about us added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 // import './App.scss';
 import Hero from './components/Hero/Hero';
 import 'tachyons';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">   
       <Hero/>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section class="section is-medium">
+      <div class="container">
+        <div className="columns multi-line">      
+          <div className="column is-6 is-offset-3">
+            <p className="is-size-4 has-text-weight-medium pv1">Masjid Al-Falah</p>
+            <p className='pv2 has-text-justified'>
+            Al-Falah sebagai sebuah masjid, telah berdiri sejak lebih dari 20 tahun yang lalu. Ia tidak hanya menjadi tempat melaksanakan ritual ibadah saja. Bahkan sejak didirikannya, ia telah menjadi pusat kegiatan sosial, pendidikan dan dakwah bagi masyarakat muslim Indonesia di Berlin dan sekitarnya.
+            </p>
+            <p className='pv2 has-text-justified'>
+            Melalui halaman ini, kami berusaha menunjukkan kepada saudara-saudara kaum muslimin di tempat lain bagaimana masjid Al-Falah Berlin dengan semua pengurus dan jamaahnya menghidupkan agama kita, Islam, di tengah budaya barat.
+            </p>
+          </div>
+          
+          <div className="column is-6 is-offset-3">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;

e1bf5f4fb43c2c7f2d2b3d615ddffaddabf51acc
faisalprady <faisalprady@gmail.com>
2019-11-12 14:45:16 +0100

    hero with nav

---
+++ src/components/Hero.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Hero = () => {
+
+  return (
+    <section className="hero is-fullheight">
+      <div className="hero-head">
+        <Navbar/>
+      </div>
+
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-vcentered">
+            <div className="column is-medium has-text-left " >
+              <p className="has-text-weight-bold is-size-4">IWKZ</p>
+              <p className="has-text-weight-medium is-size-5">Indonesisches Weisheits- und Kulturzentrum e.V.</p>
+            </div>
+            <div className="column">
+            <p>jadwal sholat</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="hero-foot">
+
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import logo from '../img/iwkz-navbar.svg'
+
+const Navbar = () => {
+  const onBurgerClick = () => {
+    document.querySelector('#navbar-toogle').classList.toggle('is-active');
+    document.querySelector('.navbar-burger').classList.toggle('is-active');
+  }
+  return (
+    <nav className="navbar is-transparent">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            <img src={logo} alt="iwkz logo" width="112" height="112" />
+          </a>
+          <div className="navbar-burger burger" data-target="navbarExampleTransparentExample" onClick={onBurgerClick}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div id="navbar-toogle" className="navbar-menu">
+          <div className="navbar-end">
+            <a className="navbar-item" href="/">
+              About Us
+            </a>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link" href="/">
+                Download
+              </a>
+              <div className="navbar-dropdown is-boxed is-right">
+                <a className="navbar-item" href="/">
+                  Jadwal Sholat
+                </a>
+                <hr className="navbar-divider"/>
+                <a className="navbar-item" href="/">
+                  Elements
+                </a>
+                <a className="navbar-item is-active" href="/">
+                  Components
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+  );
+};
+
+export default Navbar;

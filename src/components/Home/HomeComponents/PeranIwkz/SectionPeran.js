9a914932422dda8757a09101f76768cce20a6a89
faisalprady <faisalprady@gmail.com>
2020-02-04 16:07:36 +0100

    image zoom on hover

--- src/components/Home/HomeComponents/PeranIwkz/SectionPeran.js
+++ src/components/Home/HomeComponents/PeranIwkz/SectionPeran.js
@@ -1,50 +1,41 @@
 import React from "react";
-import { useInView } from "react-intersection-observer";
-import { useSpring, animated } from "react-spring";
 
 const SectionPeran = ({ content, judul, kegiatan, image, handleAccordion }) => {
-  const [ref, inView] = useInView({
-    threshold: 0.3
-  });
-  const props = useSpring({ opacity: inView ? 1 : 0 });
   return (
-    <section className="section">
-      <animated.div style={props}>
-        <div className="columns is-multiline is-vcentered" ref={ref}>
-          <div className="column is-4 is-offset-2 is-hidden-touch">
+    <section className="section ">
+      <div className="columns is-multiline is-vcentered">
+        <div className="column is-4 is-offset-2 is-hidden-touch">
+          <div className="image-wrapper">
             <img
+              className="inner-image"
               src={require(`${image}`)}
               alt={judul}
-              style={{ borderRadius: "30px" }}
             />
           </div>
-          <div className="column is-4 ">
-            <div className="content">
-              <p className="is-size-4">{judul}</p>
-              <p>{content}</p>
-              {/* <ul>
+        </div>
+        <div className="column is-4 ">
+          <div className="content">
+            <p className="is-size-4">{judul}</p>
+            <p>{content}</p>
+            {/* <ul>
                 {kegiatan.map((acara, i) => (
                   <li key={i}>{acara}</li>
                 ))}
               </ul> */}
 
-              {kegiatan.map(({ acara, penjelasan }, i) => (
-                <div key={i}>
-                  <button
-                    className="accordion"
-                    onClick={e => handleAccordion(e)}
-                  >
-                    &#8226; {acara}
-                  </button>
-                  <div className="penjelasan">
-                    <p>{penjelasan} </p>
-                  </div>
+            {kegiatan.map(({ acara, penjelasan }, i) => (
+              <div key={i}>
+                <button className="accordion" onClick={e => handleAccordion(e)}>
+                  &#8226; {acara}
+                </button>
+                <div className="penjelasan">
+                  <p>{penjelasan} </p>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
         </div>
-      </animated.div>
+      </div>
     </section>
   );
 };
--- src/components/Home/HomeComponents/PeranIwkz/SectionPeranRight.js
+++ src/components/Home/HomeComponents/PeranIwkz/SectionPeranRight.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useInView } from "react-intersection-observer";
-import { useSpring, animated } from "react-spring";
 
 const SectionPeranRight = ({
   content,
@@ -9,42 +7,35 @@ const SectionPeranRight = ({
   image,
   handleAccordion
 }) => {
-  const [ref, inView] = useInView({
-    threshold: 0.3
-  });
-  const props = useSpring({ opacity: inView ? 1 : 0 });
   return (
-    <section className="section ">
-      <animated.div ref={ref} style={props}>
-        <div className="columns is-multiline is-vcentered">
-          <div className="column is-4 is-offset-2">
-            <div className="content" style={{ margin: "auto 30px" }}>
-              <p className="is-size-4">{judul}</p>
-              <p>{content}</p>
-              {kegiatan.map(({ acara, penjelasan }, i) => (
-                <div key={i}>
-                  <button
-                    className="accordion"
-                    onClick={e => handleAccordion(e)}
-                  >
-                    &#8226; {acara}
-                  </button>
-                  <div className="penjelasan">
-                    <p>{penjelasan}</p>
-                  </div>
+    <section className="section is-medium">
+      <div className="columns is-multiline is-vcentered">
+        <div className="column is-4 is-offset-2">
+          <div className="content" style={{ margin: "auto 30px" }}>
+            <p className="is-size-4">{judul}</p>
+            <p>{content}</p>
+            {kegiatan.map(({ acara, penjelasan }, i) => (
+              <div key={i}>
+                <button className="accordion" onClick={e => handleAccordion(e)}>
+                  &#8226; {acara}
+                </button>
+                <div className="penjelasan">
+                  <p>{penjelasan}</p>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-          <div className="column is-4  is-hidden-touch">
+        </div>
+        <div className="column is-4  is-hidden-touch">
+          <div className="image-wrapper">
             <img
+              className="inner-image"
               src={require(`${image}`)}
               alt={judul}
-              style={{ borderRadius: "30px" }}
             />
           </div>
         </div>
-      </animated.div>
+      </div>
     </section>
   );
 };
